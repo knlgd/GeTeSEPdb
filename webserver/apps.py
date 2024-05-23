db66300222b8df3f542057a6a15a09da00150d13
kkkn <37575048+knlgd@users.noreply.github.com>
2024-05-23 10:51:09 +0800

    Delete webserver directory

--- webserver/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TimecourseAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "timecourse_app"
--- webserver/help_views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.db.models import Q
-
-def get_help_page(request):
-    return render(request, 'help.html')
